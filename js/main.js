076c8bdecf5fde93692775c6f8fb39800d83c51b
Israel Salinas <israel.salinas.m@gmail.com>
2022-05-28 11:25:19 -0500

    .

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/*aquí se guarda el nombre del koder*/
+let koderName
+
+let kodersArray = []
+
+/*aquí sacamos el nombre del input*/
+const getKoderName = event => {
+  koderName = event.target.value
+  console.log(koderName)
+}
+
+/*esta es la función que se ejecuta cuando escribimos en el input*/
+document.getElementById('koder-name').addEventListener('keyup', event => {
+  getKoderName(event)
+})
+
+/*esta es la función que se ejecuta cuando damos click al botón*/
+document.getElementById('add-koder').addEventListener('click', () => {
+  console.log(koderName)
+})
